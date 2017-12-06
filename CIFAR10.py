07f4fc4f7f8b2ef5048fd531c9c47e3ca8e947f9
teng <yangteng0813@gmail.com>
2017-12-06 15:01:50 -0500

    Refactor the code

--- CIFAR10.py
+++ CIFAR10.py
@@ -1,43 +1,34 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import CIFARHelper as helper
 
+CIFARPath = 'cifar-10-batches-py/'
 
-CIFAR_DIR = 'cifar-10-batches-py/'
+dirs = ['batches.meta', 'data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']
 
+inputs = [0, 1, 2, 3, 4, 5, 6]
 
+for i, path in zip(inputs, dirs):
+    inputs[i] = helper.unpickle(CIFARPath + path)
 
-
-dirs = ['batches.meta','data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5','test_batch']
-
-all_data = [0,1,2,3,4,5,6]
-
-for i,direc in zip(all_data,dirs):
-    all_data[i] = helper.unpickle(CIFAR_DIR+direc)
-
-batch_meta = all_data[0]
-data_batch1 = all_data[1]
-data_batch2 = all_data[2]
-data_batch3 = all_data[3]
-data_batch4 = all_data[4]
-data_batch5 = all_data[5]
-test_batch = all_data[6]
-
-batch_meta
-
-data_batch1.keys()
+batchMeta = inputs[0]
+batch1 = inputs[1]
+batch2 = inputs[2]
+batch3 = inputs[3]
+batch4 = inputs[4]
+batch5 = inputs[5]
+testBatch = inputs[6]
 
 
-
-X = data_batch1[b"data"]
-X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+X = batch1[b"data"]
+X = X.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).astype("uint8")
 X[0].max()
-(X[0]/255).max()
+(X[0] / 255).max()
 
 plt.imshow(X[0])
 plt.imshow(X[1])
+
 
 def one_hot_encode(vec, vals=10):
     '''
@@ -53,8 +44,8 @@ class CifarHelper():
     def __init__(self):
         self.i = 0
 
-        self.all_train_batches = [data_batch1, data_batch2, data_batch3, data_batch4, data_batch5]
-        self.test_batch = [test_batch]
+        self.all_train_batches = [batch1, batch2, batch3, batch4, batch5]
+        self.test_batch = [testBatch]
 
         self.training_images = None
         self.training_labels = None
@@ -85,31 +76,38 @@ class CifarHelper():
         self.i = (self.i + batch_size) % len(self.training_images)
         return x, y
 
+
 ch = CifarHelper()
 ch.set_up_images()
-x = tf.placeholder(tf.float32,shape=[None,32,32,3])
-y_true = tf.placeholder(tf.float32,shape=[None,10])
+x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
+y_true = tf.placeholder(tf.float32, shape=[None, 10])
 hold_prob = tf.placeholder(tf.float32)
+
 
 def init_weights(shape):
     init_random_dist = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(init_random_dist)
 
+
 def init_bias(shape):
     init_bias_vals = tf.constant(0.1, shape=shape)
     return tf.Variable(init_bias_vals)
 
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def max_pool_2by2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
+
 def convolutional_layer(input_x, shape):
     W = init_weights(shape)
     b = init_bias([shape[3]])
     return tf.nn.relu(conv2d(input_x, W) + b)
+
 
 def normal_full_layer(input_layer, size):
     input_size = int(input_layer.get_shape()[1])
@@ -117,16 +115,17 @@ def normal_full_layer(input_layer, size):
     b = init_bias([size])
     return tf.matmul(input_layer, W) + b
 
-convo_1 = convolutional_layer(x,shape=[4,4,3,32])
+
+convo_1 = convolutional_layer(x, shape=[4, 4, 3, 32])
 convo_1_pooling = max_pool_2by2(convo_1)
 
-convo_2 = convolutional_layer(convo_1_pooling,shape=[4,4,32,64])
+convo_2 = convolutional_layer(convo_1_pooling, shape=[4, 4, 32, 64])
 convo_2_pooling = max_pool_2by2(convo_2)
-convo_2_flat = tf.reshape(convo_2_pooling,[-1,8*8*64])
-full_layer_one = tf.nn.relu(normal_full_layer(convo_2_flat,1024))
-full_one_dropout = tf.nn.dropout(full_layer_one,keep_prob=hold_prob)
-y_pred = normal_full_layer(full_one_dropout,10)
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true,logits=y_pred))
+convo_2_flat = tf.reshape(convo_2_pooling, [-1, 8 * 8 * 64])
+full_layer_one = tf.nn.relu(normal_full_layer(convo_2_flat, 1024))
+full_one_dropout = tf.nn.dropout(full_layer_one, keep_prob=hold_prob)
+y_pred = normal_full_layer(full_one_dropout, 10)
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred))
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
 train = optimizer.minimize(cross_entropy)
 init = tf.global_variables_initializer()
