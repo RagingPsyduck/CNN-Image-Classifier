b51150610c1c5be70cad94f3693716d3dda1d1ae
teng <yangteng0813@gmail.com>
2017-12-06 20:37:34 -0500

    Refactor the code

--- CIFAR10.py
+++ CIFAR10.py
@@ -19,54 +19,7 @@ batch4 = inputs[4]
 batch5 = inputs[5]
 testBatch = inputs[6]
 
-
-def one_hot_encode(vec, vals=10):
-    '''
-    For use to one-hot encode the 10- possible labels
-    '''
-    n = len(vec)
-    out = np.zeros((n, vals))
-    out[range(n), vec] = 1
-    return out
-
-
-class CifarHelper():
-    def __init__(self):
-        self.i = 0
-
-        self.all_train_batches = [batch1, batch2, batch3, batch4, batch5]
-        self.test_batch = [testBatch]
-
-        self.training_images = None
-        self.training_labels = None
-
-        self.test_images = None
-        self.test_labels = None
-
-    def set_up_images(self):
-        print("Setting Up Training Images and Labels")
-
-        self.training_images = np.vstack([d[b"data"] for d in self.all_train_batches])
-        train_len = len(self.training_images)
-
-        self.training_images = self.training_images.reshape(train_len, 3, 32, 32).transpose(0, 2, 3, 1) / 255
-        self.training_labels = one_hot_encode(np.hstack([d[b"labels"] for d in self.all_train_batches]), 10)
-
-        print("Setting Up Test Images and Labels")
-
-        self.test_images = np.vstack([d[b"data"] for d in self.test_batch])
-        test_len = len(self.test_images)
-
-        self.test_images = self.test_images.reshape(test_len, 3, 32, 32).transpose(0, 2, 3, 1) / 255
-        self.test_labels = one_hot_encode(np.hstack([d[b"labels"] for d in self.test_batch]), 10)
-
-    def next_batch(self, batch_size):
-        x = self.training_images[self.i:self.i + batch_size].reshape(100, 32, 32, 3)
-        y = self.training_labels[self.i:self.i + batch_size]
-        self.i = (self.i + batch_size) % len(self.training_images)
-        return x, y
-
-ch = CifarHelper()
+ch = CIFARHelper.CifarHelper(batch1=batch1,batch2=batch2,batch3=batch3,batch4=batch4,batch5=batch5,testBatch=testBatch)
 ch.set_up_images()
 
 
--- CIFARHelper.py
+++ CIFARHelper.py
@@ -1,44 +1,49 @@
 import pickle
+import numpy as np
 
 def unpickle(file):
     with open(file, 'rb') as fo:
         cifar_dict = pickle.load(fo, encoding='bytes')
     return cifar_dict
 
+class CifarHelper():
+    def __init__(self,batch1,batch2,batch3,batch4,batch5,testBatch):
+        self.i = 0
+        self.all_train_batches = [batch1, batch2, batch3, batch4, batch5]
+        self.test_batch = [testBatch]
+        self.training_images = None
+        self.training_labels = None
+        self.test_images = None
+        self.test_labels = None
 
-def __init__(self):
-    self.i = 0
+    def set_up_images(self):
+        print("Setting Up Training Images and Labels")
 
-    self.all_train_batches = [batch1, batch2, batch3, batch4, batch5]
-    self.test_batch = [testBatch]
+        self.training_images = np.vstack([d[b"data"] for d in self.all_train_batches])
+        train_len = len(self.training_images)
 
-    self.training_images = None
-    self.training_labels = None
+        self.training_images = self.training_images.reshape(train_len, 3, 32, 32).transpose(0, 2, 3, 1) / 255
+        self.training_labels = one_hot_encode(np.hstack([d[b"labels"] for d in self.all_train_batches]), 10)
 
-    self.test_images = None
-    self.test_labels = None
+        print("Setting Up Test Images and Labels")
 
+        self.test_images = np.vstack([d[b"data"] for d in self.test_batch])
+        test_len = len(self.test_images)
 
-def set_up_images(self):
-    print("Setting Up Training Images and Labels")
+        self.test_images = self.test_images.reshape(test_len, 3, 32, 32).transpose(0, 2, 3, 1) / 255
+        self.test_labels = one_hot_encode(np.hstack([d[b"labels"] for d in self.test_batch]), 10)
 
-    self.training_images = np.vstack([d[b"data"] for d in self.all_train_batches])
-    train_len = len(self.training_images)
+    def next_batch(self, batch_size):
+        x = self.training_images[self.i:self.i + batch_size].reshape(100, 32, 32, 3)
+        y = self.training_labels[self.i:self.i + batch_size]
+        self.i = (self.i + batch_size) % len(self.training_images)
+        return x, y
 
-    self.training_images = self.training_images.reshape(train_len, 3, 32, 32).transpose(0, 2, 3, 1) / 255
-    self.training_labels = one_hot_encode(np.hstack([d[b"labels"] for d in self.all_train_batches]), 10)
-
-    print("Setting Up Test Images and Labels")
-
-    self.test_images = np.vstack([d[b"data"] for d in self.test_batch])
-    test_len = len(self.test_images)
-
-    self.test_images = self.test_images.reshape(test_len, 3, 32, 32).transpose(0, 2, 3, 1) / 255
-    self.test_labels = one_hot_encode(np.hstack([d[b"labels"] for d in self.test_batch]), 10)
-
-
-def next_batch(self, batch_size):
-    x = self.training_images[self.i:self.i + batch_size].reshape(100, 32, 32, 3)
-    y = self.training_labels[self.i:self.i + batch_size]
-    self.i = (self.i + batch_size) % len(self.training_images)
-    return x, y
+def one_hot_encode(vec, vals=10):
+    '''
+    For use to one-hot encode the 10- possible labels
+    '''
+    n = len(vec)
+    out = np.zeros((n, vals))
+    out[range(n), vec] = 1
+    return out
