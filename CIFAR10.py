d598efe03eb5b4d71c99d29ec0d32026036d00b1
teng <yangteng0813@gmail.com>
2017-12-08 14:19:18 -0500

    Referactor the code

--- CIFAR10.py
+++ CIFAR10.py
@@ -28,45 +28,15 @@ y_true = tf.placeholder(tf.float32, shape=[None, 10])
 hold_prob = tf.placeholder(tf.float32)
 
 
-def init_weights(shape):
-    init_random_dist = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(init_random_dist)
+conv1 = CIFARHelper.convolutional_layer(x, shape=[4, 4, 3, 32])
+conv1Pooling = CIFARHelper.max_pool_2by2(conv1)
 
-
-def init_bias(shape):
-    init_bias_vals = tf.constant(0.1, shape=shape)
-    return tf.Variable(init_bias_vals)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2by2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='SAME')
-
-def convolutional_layer(input_x, shape):
-    W = init_weights(shape)
-    b = init_bias([shape[3]])
-    return tf.nn.relu(conv2d(input_x, W) + b)
-
-
-def normal_full_layer(input_layer, size):
-    input_size = int(input_layer.get_shape()[1])
-    W = init_weights([input_size, size])
-    b = init_bias([size])
-    return tf.matmul(input_layer, W) + b
-
-
-convo_1 = convolutional_layer(x, shape=[4, 4, 3, 32])
-convo_1_pooling = max_pool_2by2(convo_1)
-
-convo_2 = convolutional_layer(convo_1_pooling, shape=[4, 4, 32, 64])
-convo_2_pooling = max_pool_2by2(convo_2)
-convo_2_flat = tf.reshape(convo_2_pooling, [-1, 8 * 8 * 64])
-full_layer_one = tf.nn.relu(normal_full_layer(convo_2_flat, 1024))
+conv2 = CIFARHelper.convolutional_layer(conv1Pooling, shape=[4, 4, 32, 64])
+conv2Pooling = CIFARHelper.max_pool_2by2(conv2)
+conv2Flat = tf.reshape(conv2Pooling, [-1, 8 * 8 * 64])
+full_layer_one = tf.nn.relu(CIFARHelper.normal_full_layer(conv2Flat, 1024))
 full_one_dropout = tf.nn.dropout(full_layer_one, keep_prob=hold_prob)
-y_pred = normal_full_layer(full_one_dropout, 10)
+y_pred = CIFARHelper.normal_full_layer(full_one_dropout, 10)
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred))
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
 train = optimizer.minimize(cross_entropy)
