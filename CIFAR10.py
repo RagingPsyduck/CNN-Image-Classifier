401bbc71740f3e875f435dc4695edd2f5694ede6
teng <yangteng0813@gmail.com>
2017-12-08 16:04:31 -0500

    Refactor the code

--- CIFAR10.py
+++ CIFAR10.py
@@ -3,9 +3,7 @@ import tensorflow as tf
 import CIFARHelper as CIFARHelper
 
 CIFARPath = 'cifar-10-batches-py/'
-
 dirs = ['batches.meta', 'data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']
-
 inputs = [0, 1, 2, 3, 4, 5, 6]
 
 for i, dir in zip(inputs, dirs):
@@ -22,15 +20,13 @@ testBatch = inputs[6]
 ch = CIFARHelper.CifarHelper(batch1=batch1,batch2=batch2,batch3=batch3,batch4=batch4,batch5=batch5,testBatch=testBatch)
 ch.set_up_images()
 
-
 x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
 y_true = tf.placeholder(tf.float32, shape=[None, 10])
 hold_prob = tf.placeholder(tf.float32)
 
-
-conv1 = CIFARHelper.convolutional_layer(x, shape=[4, 4, 3, 32])
+conv1 = CIFARHelper.convLayer(x, shape=[4, 4, 3, 32])
 conv1Pooling = CIFARHelper.max_pool_2by2(conv1)
-conv2 = CIFARHelper.convolutional_layer(conv1Pooling, shape=[4, 4, 32, 64])
+conv2 = CIFARHelper.convLayer(conv1Pooling, shape=[4, 4, 32, 64])
 conv2Pooling = CIFARHelper.max_pool_2by2(conv2)
 conv2Flat = tf.reshape(conv2Pooling, [-1, 8 * 8 * 64])
 full_layer_one = tf.nn.relu(CIFARHelper.normal_full_layer(conv2Flat, 1024))
@@ -47,7 +43,6 @@ with tf.Session() as sess:
         batch = ch.next_batch(100)
         sess.run(train, feed_dict={x: batch[0], y_true: batch[1], hold_prob: 0.5})
         if i % 100 == 0:
-            print('Step {}'.format(i))
             matches = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
             acc = tf.reduce_mean(tf.cast(matches, tf.float32))
-            print('Accuracy is:{}'.format(sess.run(acc, feed_dict={x: ch.test_images, y_true: ch.test_labels, hold_prob: 1.0})))
+            print('Step {} , Accuracy is:{:.4f}'.format(i,sess.run(acc, feed_dict={x: ch.test_images, y_true: ch.test_labels, hold_prob: 1.0})))
